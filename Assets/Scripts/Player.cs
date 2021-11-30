7d3f4a10da2b3ea4d1081b9e80ca256cb023a74a
eric felts <echristianfelts@gmail.com>
2021-11-30 12:06:11 -0800

    Added Player Movement Controller
    words words words.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody rig;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+
+        rig.velocity = new Vector3(x  , rig.velocity.y,z);
+
+        // Rotate to match velocity/direction of movement
+
+        Vector3 vel = rig.velocity;
+        vel.y = 0;
+
+        if (vel.x != 0 || vel.z != 0) // Rotate to match velocity BUT only if the Player is in motion.
+        {
+            transform.forward = rig.velocity;
+        }
+    }
+}
