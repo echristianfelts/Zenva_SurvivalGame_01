ddadc45e09d36df625145dc5b18911c8a68bcccc
eric felts <echristianfelts@gmail.com>
2021-11-30 12:30:48 -0800

    Refined Movement for Player
    Words Words Words

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,24 +5,47 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
     public Rigidbody rig;
+
+    private bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        // NOTE: This movement controller is set for World Space.
+        //  This will have to be changed to Player Space at some point.
+
+        // Vertical and horizontal inputs.
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
 
-        rig.velocity = new Vector3(x  , rig.velocity.y,z);
+        // Set velocity based on inputs.
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
 
         // Rotate to match velocity/direction of movement
-
+        // Create copy of velocity variable and set Y axis to be 0
         Vector3 vel = rig.velocity;
         vel.y = 0;
 
-        if (vel.x != 0 || vel.z != 0) // Rotate to match velocity BUT only if the Player is in motion.
+        // if we are moving, rotate to face our moving direction.
+        if (vel.x != 0 || vel.z != 0) // Rotate to match velocity but ONLY if the Player is in motion.
         {
-            transform.forward = rig.velocity;
+            transform.forward = vel;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            isGrounded = false;
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.contacts[0].normal == Vector3.up)
+        {
+            isGrounded = true;
         }
     }
 }
